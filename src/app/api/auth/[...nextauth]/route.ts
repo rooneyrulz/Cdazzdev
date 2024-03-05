d7fb013f86acd8bc5b65b1373e8c0048ba0977e4
ruzny <roony0782045336@gmail.com>
2024-03-05 22:25:44 +0530

    api route options

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,5 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import User from "@/models/User";
-import connect from "@/config/db";
-import { z } from "zod";
-import { loginSchema } from "@/app/validationSchema";
+import { authOptions } from "./options"
 
-type LoginForm = z.infer<typeof loginSchema>;
-
-export const authOptions: any = {
-  providers: [
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials: LoginForm) {
-        await connect();
-        try {
-          const user = await User.findOne({ email: credentials.email });
-
-          if (!user) {
-            throw new Error("Invalid credentials!");
-          }
-
-          const isMatch = await bcrypt.compare(
-            credentials.password,
-            user.password
-          );
-
-          if (!isMatch) {
-            throw new Error("Invalid credentials!");
-          }
-
-          return user;
-        } catch (err: any) {
-          throw new Error(err);
-        }
-      },
-    }),
-  ],
-};
-
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
