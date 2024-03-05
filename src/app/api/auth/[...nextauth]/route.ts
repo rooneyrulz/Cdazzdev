ffa3b4c3c6b90f366553e8d3fcbd31533801d9cd
ruzny <roony0782045336@gmail.com>
2024-03-05 22:45:37 +0530

    next-auth config updated

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,49 @@
-import NextAuth from "next-auth/next";
-import { authOptions } from "./options"
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { z } from "zod";
+
+import User from "@/models/User";
+import connect from "@/config/db";
+import { loginSchema } from "@/app/validationSchema";
+
+type LoginForm = z.infer<typeof loginSchema>;
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: LoginForm) {
+        await connect();
+        try {
+          const user = await User.findOne({ email: credentials.email });
+
+          if (!user) {
+            throw new Error("Invalid credentials!");
+          }
+
+          const isMatch = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isMatch) {
+            throw new Error("Invalid credentials!");
+          }
+
+          return user;
+        } catch (err: any) {
+          throw new Error(err);
+        }
+      },
+    }),
+  ],
+};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
