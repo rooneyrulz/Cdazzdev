b5b57ab3b660f78fd1ba845b38c889fc5cebe025
ruzny <roony0782045336@gmail.com>
2024-03-05 21:52:11 +0530

    added .env.example | navbar updated

--- src/app/navbar/NavBar.tsx
+++ src/app/navbar/NavBar.tsx
@@ -1,17 +1,14 @@
 import Link from "next/link";
 import { FaHouseDamage } from "react-icons/fa";
-import { getServerSession } from "next-auth";
 import NavLink from "./NavLink";
 
 const NavBar = async () => {
-  const session = await getServerSession();
-
   return (
     <nav className="flex space-x-8 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
         <FaHouseDamage size={32} />
       </Link>
-      <NavLink session={session} />
+      <NavLink/>
     </nav>
   );
 };
--- src/app/navbar/NavLink.tsx
+++ src/app/navbar/NavLink.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { ReactNode } from "react";
 import Link from "next/link";
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowRight, FaUserAlt } from "react-icons/fa";
 import { usePathname } from "next/navigation";
 import classNames from "classnames";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 type LinkType = {
   label: string;
@@ -14,18 +14,17 @@ type LinkType = {
   onClick?: () => void;
 };
 
-export default function NavLink({ session }: any) {
+export default function NavLink() {
   const currentPath = usePathname();
+  const { data: session, status: sessionStatus } = useSession();
 
   const links: LinkType[] = [
-    { label: "Home", href: "/" },
     ...(session
       ? [
-          { label: "Profile", href: "/profile", private: true },
+          { label: "Profile", href: "/profile", icon: <FaUserAlt /> },
           {
             label: "Logout",
             href: "",
-            private: true,
             onClick: () => signOut(),
           },
         ]
@@ -36,21 +35,35 @@ export default function NavLink({ session }: any) {
   ];
 
   return (
-    <ul className="flex space-x-6">
-      {links.map((link) => (
-        <Link
-          key={link.href}
-          className={classNames({
-            "text-zinc-900": link.href === currentPath,
-            "text-zinc-500": link.href !== currentPath,
-            "hover:text-zinc-800 transition-colors": true,
-          })}
-          href={link.href}
-          onClick={link?.onClick}
-        >
-          {link.label}
-        </Link>
-      ))}
-    </ul>
+    sessionStatus !== "loading" && (
+      <div className="flex space-x-6 w-[100%]">
+        <ul className="flex space-x-6 mr-auto">
+          <Link
+            href="/"
+            className={`${
+              currentPath === "/" ? "text-zinc-900" : "text-zinc-500"
+            } hover:text-zinc-800 transition-colors`}
+          >
+            Home
+          </Link>
+        </ul>
+        <ul className="flex space-x-6">
+          {links.map((link) => (
+            <Link
+              key={link.href}
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+              href={link.href}
+              onClick={link?.onClick}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+      </div>
+    )
   );
 }
